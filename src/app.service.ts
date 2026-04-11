20a6c310d81ae7d3b4faf31e68065892f52994a3
akuma19 <kellyhandy31@gmail.com>
2026-04-11 22:16:47 +0100

    feat: prisma setup

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
 
 @Injectable()
 export class AppService {
+  constructor(private prisma: PrismaService) {}
+
   getHello(): string {
     return 'Hello World!';
   }
+
+  // Example method to get users (uncomment when you have the User model)
+  // async getUsers() {
+  //   return this.prisma.user.findMany();
+  // }
 }
