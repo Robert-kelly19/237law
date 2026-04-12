0b0de368387b56ca7298ac473fedafa3620108db
akuma19 <kellyhandy31@gmail.com>
2026-04-12 21:35:58 +0100

    added pdfs

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma.service';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
-
+const Port = process.env.PORT ?? 3000;
   // Enable shutdown hooks
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(Port, () => {
+    logger.log(`Server is running on port ${Port}`);
+  });
 }
 bootstrap();
