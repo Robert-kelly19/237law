800451dc00495cb136048b4a27c1eae31482eed0
akuma19 <kellyhandy31@gmail.com>
2026-05-01 18:52:42 +0100

    feat:add whatsapp controller and service to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,17 @@ import { PdfService } from './pdf.service';
 import { EmbeddingService } from './embedding.service';
 import { RagService } from './rag.service';
 import { RagController } from './rag.controller';
+import { WhatsappController } from './whatsapp/whatsapp.controller';
+import { WhatsappService } from './whatsapp/whatsapp.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController, RagController],
-  providers: [AppService, PrismaService, PdfService, EmbeddingService, RagService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
+  controllers: [AppController, RagController, WhatsappController],
+  providers: [AppService, PrismaService, PdfService, EmbeddingService, RagService, WhatsappService],
 })
 export class AppModule {}
