9efaeb859d028ac81b68de453331d2fadc6f36a9
akuma19 <kellyhandy31@gmail.com>
2026-06-04 17:12:47 +0100

    migration

---
+++ prisma/migrations/20260604160941_speed/migration.sql
@@ -0,0 +1,20 @@
+-- DropIndex
+DROP INDEX "agent_reasoning_conversation_step_idx";
+
+-- DropIndex
+DROP INDEX "conversation_turns_user_session_time_idx";
+
+-- DropIndex
+DROP INDEX "law_sections_content_hash_idx";
+
+-- DropIndex
+DROP INDEX "law_sections_embedding_hnsw";
+
+-- DropIndex
+DROP INDEX "law_sections_source_idx";
+
+-- DropIndex
+DROP INDEX "semantic_memory_user_key_idx";
+
+-- DropIndex
+DROP INDEX "semantic_memory_user_type_importance_idx";
